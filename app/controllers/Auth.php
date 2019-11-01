a7deaea43ce950193bb6558a0f36b28fb0887cfb
Callum Macpherson <callummac@protonmail.com>
2019-11-01 16:08:28 +0000

    Added register rotue

--- app/controllers/Auth.php
+++ app/controllers/Auth.php
@@ -62,24 +62,51 @@
         }
 
         public function getRegister() {
-            View::render('register');
+            $data = [
+                "email" => "",
+                "confirm_email" => "",
+                "password" => "",
+                "confirm_password" => "",
+                "errors" => []
+            ];
+
+            View::render('register', $data);
         }
 
         public function postRegister() {
-            
-        }
+            $data = [
+                "email" => filter_var($_POST['email'], FILTER_SANITIZE_EMAIL),
+                "confirm_email" => filter_var($_POST['confirm_email'], FILTER_SANITIZE_EMAIL),
+                "password" => filter_var($_POST['password'], FILTER_SANITIZE_STRING),
+                "confirm_password" => filter_var($_POST['confirm_password'], FILTER_SANITIZE_STRING),
+                "errors" => []
+            ];
 
-        public function login() {
-            
+            if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $data['errors'][] = 'Enter a valid email.';
+            } else if ($data['email'] !== $data['confirm_email']) {
+                $data['errors'][] = 'Email field must match.';
+            }
 
-            
+            if (!(strlen($data['password']) >= 8 && strlen($data['password']) <= 100)) {
+                $data['errors'][] = 'Password must be between 8-100 characters.';
+            } else if ($data['password'] !== $data['confirm_password']) {
+                $data['errors'][] = 'Password field must match.';
+            }
 
-        }
+            // Check if valid data was sent
+            if (sizeof($data['errors']) > 0) View::render('register', $data);
 
-        public function register() {
 
+            $response = $this->authModel->createUser($data['email'], $data['password']);
 
-            // echo 'register page';
+            if (!response) {
+                $data['errors'][] = 'Something went wrong.';
+                View::render('register', $data);
+            }
+
+            // Success
+            header('Location: ' . URLROOT . '/login');
         }
 
         public function logout() {
